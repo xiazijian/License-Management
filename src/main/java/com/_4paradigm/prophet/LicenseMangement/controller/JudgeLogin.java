d134ade71b1c3096ad51aa58b04b2991c039fd65
Xia Weiyi <xiaweiyi@4paradigm.com>
2018-12-24 16:32:38 +0800

    add JudgeLogin controller

---
+++ src/main/java/com/_4paradigm/prophet/LicenseMangement/controller/JudgeLogin.java
@@ -0,0 +1,30 @@
+package com._4paradigm.prophet.LicenseMangement.controller;
+
+import com._4paradigm.prophet.rest.model.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@CrossOrigin
+@RestController
+public class JudgeLogin {
+    @GetMapping(value = "judgeSession")
+    public ResponseEntity<Response> juedeSession(HttpServletRequest httpServletRequest){
+        ResponseEntity<Response> responseEntity;
+        Response res = new Response();
+        Object user = httpServletRequest.getSession().getAttribute("user");
+        if(user==null){
+            res.setMsg("没有登录，请重新登录");
+            responseEntity = new ResponseEntity<>(res, HttpStatus.FORBIDDEN);
+        }
+        else{
+            res.setMsg("已登录");
+            responseEntity = new ResponseEntity<>(res, HttpStatus.OK);
+        }
+        return responseEntity;
+    }
+}
