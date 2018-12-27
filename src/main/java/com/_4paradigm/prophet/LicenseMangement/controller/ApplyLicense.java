6f4865eff6b1936e3adcb1d2fd84e7be27af845d
Xia Weiyi <xiaweiyi@4paradigm.com>
2018-12-27 14:23:18 +0800

    add date format judgement

--- src/main/java/com/_4paradigm/prophet/LicenseMangement/controller/ApplyLicense.java
+++ src/main/java/com/_4paradigm/prophet/LicenseMangement/controller/ApplyLicense.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Map;
 @CrossOrigin
@@ -26,20 +28,31 @@ public class ApplyLicense {
         System.out.println(httpEntity.getBody());
         System.out.println("json:"+JSON.toJSONString(httpEntity.getBody()));
         Map json = (Map) httpEntity.getBody();
+        String IssuedDate = (String) json.get("IssuedDate");
+        String ExpiredDate = (String) json.get("ExpiredDate");
+        System.out.println(IssuedDate+"|"+ExpiredDate);
         String config = JSON.toJSONString(httpEntity.getBody());
         String result=null;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        simpleDateFormat.setLenient(false);
         try{
-            result = JudgeProduct.judgeProductGenerateLicense(json);
-            Map<String,String> map = new HashMap<>();
-            map.put("name", (String) json.get("username"));
-            map.put("license",result);
-            map.put("configuration",config.substring(1,config.length()-1));
-            insertLicenseRecord.insert(map);
+            simpleDateFormat.parse(IssuedDate);
+            simpleDateFormat.parse(ExpiredDate);
+            try{
+                result = JudgeProduct.judgeProductGenerateLicense(json);
+                Map<String,String> map = new HashMap<>();
+                map.put("name", (String) json.get("username"));
+                map.put("license",result);
+                map.put("configuration",config.substring(1,config.length()-1));
+                insertLicenseRecord.insert(map);
+                System.out.println(result);
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        }catch (ParseException e){
+            response.setStatus(500);
         }
-        catch(Exception e){
-            e.printStackTrace();
-        }
-        System.out.println(result);
         return result;
     }
 }
